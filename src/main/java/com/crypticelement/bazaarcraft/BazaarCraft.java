d58fa0f96b419eed431bd71f8d85a8187657990d
Jason Gagner <gagner.jason@pm.me>
2022-09-28 15:36:10 -0400

    Initial forge project setup

---
+++ src/main/java/com/crypticelement/bazaarcraft/BazaarCraft.java
@@ -0,0 +1,17 @@
+package com.crypticelement.bazaarcraft;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(BazaarCraft.MODID)
+public class BazaarCraft
+{
+    public static final String MODID = "bazaarcraft";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BazaarCraft()
+    {
+    }
+}
